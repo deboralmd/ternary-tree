75234feca5d4216615e3c8b64d5277d5b980ecc4
ReJJCM <rers162@gmail.com>
2017-06-19 13:26:47 -0300

    Finished read file method

--- ReadFile.java
+++ ReadFile.java
@@ -1,40 +1,79 @@
 package pucrs.alpro3;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
-public class ReadFile 
-{
-	
-	public ReadFile(){}
-		
-	public void LerFile()
+	import java.io.IOException;
+	import java.nio.file.Files;
+	import java.nio.file.Path;
+	import java.nio.file.Paths;
+	import java.util.Scanner;
+
+	public class ReadFile 
 	{
-		 String[] root,left,center, right;
-		 root = left = center = right = null;
-		 Path pt = Paths.get("F_1.txt");
+		Integer[] root, root2, left,center, center2, right;
 		
-		 try (Scanner sc = new Scanner(Files.newBufferedReader(pt)))
-		 {
-			 int x = 0, first = sc.nextInt();
-	
-			 while(sc.hasNext())
+		public ReadFile(){}
+			
+		public void LerFile()
+		{
+			 
+			
+
+			 Path pt = Paths.get("F_1.txt");
+			
+			 try (Scanner sc = new Scanner(Files.newBufferedReader(pt)))
 			 {
-				 if (x < first)
+				 int second = 0, x = 0, first = sc.nextInt();
+				 
+			root = new Integer[first];
+			left = new Integer[first];
+			center = new Integer[first];
+				 
+			
+				 while(sc.hasNext())
 				 {
-					 String line = sc.nextLine();
-					 String[] temp = line.split(" ");
-					 root[0] = temp[0];
-					 left[0] = temp[2];
-				 }				 
-			 }			 
-		 }
-		 catch(IOException ex)
-     {
-        System.out.println(ex);
-		 }	 
+					 if (x < first)
+					 {
+						 String line = sc.nextLine();
+						 String[] temp = line.split(" ");
+						 
+						 root[0] = Integer.parseInt(temp[0]);
+						 left[0] = Integer.parseInt(temp[1]);
+						 center[0] = Integer.parseInt(temp[2]);
+						 
+					 }				 
+					 
+					 if(x == first + 1)
+						 second = sc.nextInt();
+					root2 = new Integer[first];
+					right = new Integer[first];
+					center2 = new Integer[first];
+					 
+					 
+					 if(x > second)
+					 {
+						 String line = sc.nextLine();
+						 String[] temp = line.split(" ");
+						 root2[0] = Integer.parseInt(temp[0]);
+						 right[0] = Integer.parseInt(temp[1]);
+						 center2[0] = Integer.parseInt(temp[2]);
+						 
+					 }
+						 
+					 x++; 
+					 
+				 }			 
+			 }
+			 catch(IOException ex)
+	     {
+	        System.out.println(ex);
+			 }	 
+		}
+		
+		
+	public void juntarArvores()
+	{
+
+		
 	}
-}
+	}
+
